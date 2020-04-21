106c505299d5b79b54834e142c08453daff6410e
wesleygas <wesleygl16@gmail.com>
2020-04-21 20:23:42 -0300

    baisc_interactions

---
+++ Assets/scripts/godCapsule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class godCapsule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer rend;
+    public float Speed = 1;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.SetColor("_Color", Color.HSVToRGB( Mathf.PingPong(Time.time * Speed, 1), 1, 1));
+    }
+}
